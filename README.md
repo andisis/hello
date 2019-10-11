9f0f071ed50ea7af402811b4dddfa4a2c3fe0a85
Andi Siswanto <andisis92@gmail.com>
2019-10-11 22:34:57 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# HELLO
+
+[![Build Status](http://35.188.26.255/api/badges/andisis/hello/status.svg)](http://35.188.26.255/andisis/hello)
+
+Simple app built in golang
---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const resp = "Hai, nama saya %s"
+
+func main() {
+	http.HandleFunc("/", greetingHandler)
+	log.Fatal(http.ListenAndServe(":80", nil))
+}
+
+func greetingHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, resp, r.URL.Path[1:])
+}
